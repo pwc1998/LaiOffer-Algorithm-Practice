32c85e358ae9fae2e3d97f58e1b938d5f3d10347
pwc1998 <chengpeiwen0514@gmail.com>
2021-09-09 01:51:50 -0400

    update

--- src/Algorithms/HeapNGraph.java
+++ src/Algorithms/HeapNGraph.java
@@ -11,7 +11,6 @@ public class HeapNGraph {
 //        printInt(5);
 //        int[] arr = {1,2,3,8,5,6,4,7,9};
         printInt(new Integer(args[0]));
-        printInt(3);
         int[] arr = {6,5,4,3,2,1};
         int[][] matrix = {{1,3,5,7},
                 {2,4,8,9},
