6480b1d775d3233a9cd8ca4c7805fed61b87be56
Pwc <p6cheng@ucsd.edu>
2021-09-09 14:35:30 -0400

    Update

---
+++ src/Algorithms/DFS.java
@@ -0,0 +1,30 @@
+package Algorithms;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class DFS {
+    public static void main(String[] args) {
+
+    }
+    private static void printInt(int num) {
+        System.out.println(num);
+    }
+    private static void printArray(int[] arr) {
+        System.out.println(Arrays.toString(arr));
+    }
+    private static void printList(List<Integer > list) {
+        System.out.println(list.toString());
+    }
+    private static void printBool(Boolean bool) {
+        System.out.println(bool);
+    }
+
+    /*
+     * TWO questions for DFS:
+     * 1. how many levels are in the recursion tree? NOT input tree
+     * 2. how many different states would there be for each level?
+     */
+
+
+}
